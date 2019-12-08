a4face12834527fd8c26e5d97643610ea98564e1
swayamraina <swayamraina@gmail.com>
2019-12-08 08:37:18 +0530

    added logging

--- payment-assistant-bot/src/main/java/com/grabathon/paymentassistant/web/api/controller/StepsController.java
+++ payment-assistant-bot/src/main/java/com/grabathon/paymentassistant/web/api/controller/StepsController.java
@@ -1,50 +1,39 @@
 package com.grabathon.paymentassistant.web.api.controller;
 
-import com.grabathon.paymentassistant.web.api.request.NextStepsRequest;
-import com.grabathon.paymentassistant.web.api.request.PreviousStepsRequest;
 import com.grabathon.paymentassistant.web.api.response.NextStepsResponse;
-import com.grabathon.paymentassistant.web.api.response.PreviousStepsResponse;
 import com.grabathon.paymentassistant.web.api.response.base.Response;
 import com.grabathon.paymentassistant.web.api.routes.APIRoutes;
 import com.grabathon.paymentassistant.web.api.service.PaymentAssistanceService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import static com.grabathon.paymentassistant.utils.Constants.EMPTY;
-import static com.grabathon.paymentassistant.web.api.controller.utils.Constants.HEADERS.CLIENT_ID;
-import static com.grabathon.paymentassistant.web.api.controller.utils.Constants.HEADERS.USER_ID;
 import static com.grabathon.paymentassistant.web.api.controller.utils.Constants.STEP_ID;
 import static com.grabathon.paymentassistant.web.api.response.base.Code.SUCCESS;
 
 @RestController @RequestMapping( APIRoutes.V1 ) public class StepsController {
 
+    private static final Logger logger = LogManager.getLogger(StepsController.class);
 
     @Autowired private PaymentAssistanceService paymentAssistanceService;
 
 
     @PostMapping ( APIRoutes.NEXT_STEPS ) public Response<NextStepsResponse> getNextSteps (
-            @RequestHeader(name = CLIENT_ID, defaultValue = EMPTY) String client,
-            @RequestHeader(name = USER_ID, defaultValue = EMPTY) String user,
-            @PathVariable(name = STEP_ID) Long id,
-            @RequestBody NextStepsRequest request) {
+            @PathVariable(name = STEP_ID) String i,
+            @RequestBody String in) {
 
+        JSONObject request = new JSONObject(in);
+        Long id = Long.parseLong(i);
+        logger.info("request url:: bot.com/v1/" + i + "/next/steps");
+        logger.info("request body:: " + request.toString());
         NextStepsResponse response = paymentAssistanceService.serve(id, request);
         return new Response <> (
                 SUCCESS.getCode(),
                 String.format(SUCCESS.getMessage(), id),
                 response
         );
-    }
-
-
-    @PostMapping ( APIRoutes.PREVIOUS_STEPS ) public Response<PreviousStepsResponse> getPreviousSteps (
-            @RequestHeader(name = CLIENT_ID, defaultValue = EMPTY) String client,
-            @RequestHeader(name = USER_ID, defaultValue = EMPTY) String user,
-            @PathVariable(name = STEP_ID) String id,
-            @RequestBody PreviousStepsRequest request) {
-
-
-        return null;
     }
 
 
