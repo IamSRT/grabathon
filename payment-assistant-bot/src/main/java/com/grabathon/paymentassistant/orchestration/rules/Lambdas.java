12b36402bd23d13e101cc32f1779a3a74f4748d3
swayamraina <swayamraina@gmail.com>
2019-12-08 08:36:58 +0530

    added rules and lambdas

--- payment-assistant-bot/src/main/java/com/grabathon/paymentassistant/orchestration/rules/Lambdas.java
+++ payment-assistant-bot/src/main/java/com/grabathon/paymentassistant/orchestration/rules/Lambdas.java
@@ -2,64 +2,51 @@ package com.grabathon.paymentassistant.orchestration.rules;
 
 import com.google.gson.Gson;
 import com.grabathon.paymentassistant.http.HTTP;
-import com.grabathon.paymentassistant.web.api.request.base.StepsRequest;
-import com.grabathon.paymentassistant.web.api.request.base.Vouch;
 import org.json.JSONObject;
-
-import java.util.LinkedList;
-import java.util.List;
 
 public class Lambdas {
      private static final String walletServiceHost  = "http://192.168.43.141:4000/api/";
      private static final String Success  = "Success";
 
-     public static Float GetBalance(StepsRequest stepsRequest){
-         String url = walletServiceHost + "user/" +stepsRequest.requesterNumber;
+     public static Float GetBalance(JSONObject stepsRequest){
+         String url = walletServiceHost + "user/" +stepsRequest.getString("UserId");
          JSONObject response = HTTP.get(url);
          if (response == null){return (float) -1;}
          return  response.getJSONObject("data").getJSONObject("wallet").getFloat("balance");
      }
 
-    public static String GetUser(StepsRequest stepsRequest){
-        String url = walletServiceHost + "user/" +stepsRequest.UserId;
+    public static String GetUser(JSONObject stepsRequest){
+        String url = walletServiceHost + "user/" +stepsRequest.getString("UserId");
         JSONObject response = HTTP.get(url);
         if (response == null){return "{}";}
         return  response.toString();
     }
 
-    public static boolean requestVouch (StepsRequest stepsRequest){
+    public static boolean requestVouch (JSONObject stepsRequest){
         String url = walletServiceHost + "user/vouch";
-        List<Vouch> vouches = new LinkedList<>();
-        for (String v : stepsRequest.requesteeNumber) {
-            Vouch vouch = new Vouch();
-            vouch.VoucheeId = stepsRequest.requesterNumber;
-            vouch.VoucherId = v;
-            vouch.Amount = stepsRequest.amount;
-            vouches.add(vouch);
-        }
         String data = new Gson().toJson(stepsRequest);
         JSONObject response = HTTP.post(url,data);
         if (response == null){return false;}
         return response.getString("message").equals(Success);
     }
 
-    public static boolean acceptVouch(StepsRequest stepsRequest){
-        String url = walletServiceHost + "user/vouch" + stepsRequest.vouch_id + "/accept";
+    public static boolean acceptVouch(JSONObject stepsRequest){
+        String url = walletServiceHost + "user/vouch" + stepsRequest.getString("vouch_id") + "/accept";
         String data = new Gson().toJson(stepsRequest);
         JSONObject response = HTTP.post(url,data);
         if (response == null){return false;}
         return response.getString("message").equals(Success);
     }
 
-    public static boolean declineVouch(StepsRequest stepsRequest){
-        String url = walletServiceHost + "user/vouch/" + stepsRequest.vouch_id + "/reject";
+    public static boolean declineVouch(JSONObject stepsRequest){
+        String url = walletServiceHost + "user/vouch/" + stepsRequest.getString("vouch_id") + "/reject";
         String data = new Gson().toJson(stepsRequest);
         JSONObject response = HTTP.post(url,data);
         if (response == null){return false;}
         return response.getString("message").equals(Success);
     }
 
-    public static boolean EnableAutoPay(StepsRequest stepsRequest){
+    public static boolean EnableAutoPay(JSONObject stepsRequest){
         String url = walletServiceHost + "user/enable-auto_pay";
         String data = new Gson().toJson(stepsRequest);
         JSONObject response = HTTP.post(url,data);
@@ -67,34 +54,40 @@ public class Lambdas {
         return response.getString("message").equals(Success);
     }
 
-    public static boolean IsAutoPayEnabled(StepsRequest stepsRequest){
+    public static boolean IsAutoPayEnabled(JSONObject stepsRequest){
         String url = walletServiceHost + "user/is-auto-pay-enabled";
-        String data = new Gson().toJson(stepsRequest.Vouch);
+        String data = new Gson().toJson(stepsRequest.getJSONObject("Vouch"));
         JSONObject response = HTTP.post(url,data);
         if (response == null){return false;}
         return response.getString("message").equals(Success);
     }
 
-    public static boolean IsVouchValid(StepsRequest stepsRequest){
+    public static boolean IsVouchValid(JSONObject stepsRequest){
         String url = walletServiceHost + "user/is-vouch-valid";
-        String data = new Gson().toJson(stepsRequest.Vouch);
+        String data = new Gson().toJson(stepsRequest.getJSONObject("Vouch"));
         JSONObject response = HTTP.post(url,data);
         if (response == null){return false;}
         return response.getString("message").equals(Success);
     }
 
-    public static boolean CreatePayment(StepsRequest stepsRequest){
+    public static boolean CreatePayment(JSONObject stepsRequest){
         String url = walletServiceHost + "payment/create";
-        String data = new Gson().toJson(stepsRequest.payment);
+        String data = new Gson().toJson(stepsRequest.getJSONObject("payment"));
         JSONObject response = HTTP.post(url,data);
         if (response == null){return false;}
         return response.getString("message").equals(Success);
     }
 
-    public static String GetPayment(StepsRequest stepsRequest){
-        String url = walletServiceHost + "payment/"+stepsRequest.PaymentId;
+    public static String GetPayment(JSONObject stepsRequest){
+        String url = walletServiceHost + "payment/"+stepsRequest.get("PaymentId");
         JSONObject response = HTTP.get(url);
         if (response == null){return "{}";}
         return response.toString();
+    }
+
+    public static JSONObject GetPendingPayment(JSONObject stepsRequest){
+        String url = walletServiceHost + "user/"+stepsRequest.get("UserId")+"/pending-payments";
+        JSONObject response = HTTP.get(url);
+        return response;
     }
 }
--- payment-assistant-bot/src/main/java/com/grabathon/paymentassistant/orchestration/rules/Rules.java
+++ payment-assistant-bot/src/main/java/com/grabathon/paymentassistant/orchestration/rules/Rules.java
@@ -1,11 +1,19 @@
 package com.grabathon.paymentassistant.orchestration.rules;
 
 import com.grabathon.paymentassistant.storage.db.wrapper.Step;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
 @Service public class Rules {
 
-    public boolean check (Step step) {
+    public boolean check (Step step, JSONObject request) {
+        switch (step.getRule()) {
+            case "BALANCE_CHECK":
+                return Lambdas.GetBalance(request) > 0.0;
+
+            case "AUTO_PAY_NOT_ENABLED":
+                return !Lambdas.IsAutoPayEnabled(request);
+        }
         return true;
     }
 
